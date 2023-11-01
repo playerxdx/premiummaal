2f65e891cff3f78c9b91b8e3e15e0a86bf271641
ｒｙｍｅ <ryme.care@hotmail.com>
2023-11-01 14:28:51 +0530

    updated

--- bot.py
+++ bot.py
@@ -11,14 +11,14 @@ nltk.download("words")
 
 API_ID = os.environ.get("API_ID") 
 API_HASH = os.environ.get("API_HASH") 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+SESSION = os.environ.get("SESSION")
 
 # Bot
 app = Client(
     "word9",
     api_id=API_ID,
     api_hash=API_HASH,
-    bot_token=BOT_TOKEN
+    session_string=SESSION
 )
 
 # flask server
@@ -33,14 +33,12 @@ def home():
 starting_letter_pattern = r"start with ([A-Z])"
 min_length_pattern = r"include at least (\d+) letters"
 time_limit_pattern = r"You have (\d+)s to answer."
-game_start_pattern = r"30s left to /join."
-your_turn_pattern = r"Turn: Word9 Helper"
 
-@app.on_message(filters.command("start"))
+@app.on_message(filters.command("start"), prefixes=".")
 async def start(client, message):
-    await message.reply("Hi, I can play word9 game with you")
+    await message.edit("Hi, I can play word9 game with you")
 
-@app.on_message(filters.text)
+@app.on_message(filters.text )
 def handle_incoming_message(client, message):
     puzzle_text = message.text
 
@@ -48,28 +46,23 @@ def handle_incoming_message(client, message):
     min_length_match = re.search(min_length_pattern, puzzle_text)
     time_limit_match = re.search(time_limit_pattern, puzzle_text)
 
-    if re.search(game_start_pattern, puzzle_text):
-        client.send_message(message.chat.id, "/join")
-        return
+    if starting_letter_match and min_length_match and time_limit_match:
+        starting_letter = starting_letter_match.group(1)
+        min_length = int(min_length_match.group(1))
 
-    if re.search(your_turn_pattern, puzzle_text):
-        if starting_letter_match and min_length_match and time_limit_match:
-            starting_letter = starting_letter_match.group(1)
-            min_length = int(min_length_match.group(1))
+        english_words = set(nltk.corpus.words.words())
 
-            english_words = set(nltk.corpus.words.words())
+        valid_words = [word for word in english_words if word.startswith(starting_letter) and len(word) >= min_length]
 
-            valid_words = [word for word in english_words if word.startswith(starting_letter) and len(word) >= min_length]
+        if valid_words:
+            random_word = random.choice(valid_words)
 
-            if valid_words:
-                random_word = random.choice(valid_words)
-
-                response_message = f"{random_word}"
-                client.send_message(message.chat.id, response_message)
-            else:
-                client.send_message(message.chat.id, "No valid words found for the given criteria.")
+            response_message = f"{random_word}"
+            client.send_message(message.chat.id, response_message)
         else:
-            print("Criteria not found in the puzzle text.")
+            client.send_message(message.chat.id, "No valid words found for the given criteria.")
+    else:
+        print("Criteria not found in the puzzle text.")
     return        
 
 def run():
