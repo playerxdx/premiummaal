0bf928a247fdc4f48a13a6adf5028f45833b8cee
ｒｙｍｅ <ryme.care@hotmail.com>
2023-11-05 15:33:26 +0530

    updated

--- bot.py
+++ bot.py
@@ -29,7 +29,7 @@ def home():
 
 starting_letter_pattern = r"start with ([A-Z])"
 min_length_pattern = r"include at least (\d+) letters"
-trigger_pattern = r"Turn: ᖇᗩᕼᑌᒪ.*" # Replace with your own trigger pattern (Your telegram profile name)
+trigger_pattern = r"Turn: ᖇᗩᕼᑌᒪ.*" # Replace "ᖇᗩᕼᑌᒪ" with your own trigger pattern (Your telegram profile name)
 
 
 @app.on_message(filters.me & filters.command("ping", prefixes="!"))
